77dff55678bf44b9246d6a5cd481beef154e8717
leiyunkang <leiyunkang7@outlook.com>
2020-04-12 14:45:43 +0800

    更新依赖

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,41 @@
 <template>
   <div class="home">
-    {{ state }}
-
-    <el-button @click="refetch">refetch</el-button>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-import useAxios from '@/composables/use-axios'
+import { defineComponent, ref } from '@vue/composition-api'
 
 export default defineComponent({
   setup() {
-    const { state, refetch } = useAxios(
+    const tableData = ref([
       {
-        url: 'http://localhost:8080/',
-        method: 'get'
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
       },
-      { manual: true }
-    )
-
-    return { state, refetch }
+      {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      },
+      {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      },
+      {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }
+    ])
+    return { tableData }
   }
 })
 </script>
